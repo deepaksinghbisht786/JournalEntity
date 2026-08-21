219964c8806eab4dd7a84dc908fadd63c7f98d21
Deepak <bishtdpk35@gmail.com>
2026-08-21 16:39:08 +0530

    added delete by id feature

--- src/main/java/com/example/demo/Component/JournalComponent_user.java
+++ src/main/java/com/example/demo/Component/JournalComponent_user.java
@@ -2,7 +2,6 @@ package com.example.demo.Component;
 
 
 import com.example.demo.Entity.Entity;
-import com.example.demo.Entity.User;
 import com.example.demo.Service.Journal_Service;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,26 +17,32 @@ public class JournalComponent_user {
     private Journal_Service service;
 
     @PostMapping("/{user}")
-    public String createuserJournal(@RequestBody Entity entity, @PathVariable String user){
-        return service.createJournal(entity,user);
+    public String createuserJournal(@RequestBody Entity entity, @PathVariable String user) {
+        return service.createJournal(entity, user);
     }
 
     @GetMapping("/{user}")
-    public ResponseEntity<List<Entity>> getuserJour(@PathVariable String user){
+    public ResponseEntity<List<Entity>> getuserJour(@PathVariable String user) {
         return service.getuserJournals(user);
     }
+
     @DeleteMapping("/{user}")
-    public String delete(@PathVariable String user){
+    public String delete(@PathVariable String user) {
         return service.getUserJournaldel(user);
     }
 
     @DeleteMapping("deleteall")
-    public String deleteallthings(){
-           return service.deltealll();
+    public String deleteallthings() {
+        return service.deltealll();
     }
 
     @PutMapping("/{userr}/{id}")
-    public  String updatejournal(@PathVariable String userr, @PathVariable ObjectId id, @RequestBody Entity abc){
-        return service.updateJournals(userr,id,abc);
+    public String updatejournal(@PathVariable String userr, @PathVariable ObjectId id, @RequestBody Entity abc) {
+        return service.updateJournals(userr, id, abc);
+    }
+
+    @DeleteMapping("{user}/{id}")
+    public String deleteJounalbyid(ObjectId id, String user) {
+        return service.deletebyId(id, user);
     }
 }
--- src/main/java/com/example/demo/Service/Journal_Service.java
+++ src/main/java/com/example/demo/Service/Journal_Service.java
@@ -7,7 +7,6 @@ import com.example.demo.Repo.Journalrepo;
 import com.example.demo.Repo.UserRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +15,12 @@ import java.util.Optional;
 
 @Service
 public class Journal_Service {
-    @Autowired private UserRepo repoo;
+    @Autowired
+    private UserRepo repoo;
 
 
-    @Autowired private Journalrepo journalrepo;
+    @Autowired
+    private Journalrepo journalrepo;
 
     public String createJournal(Entity abc, String username) {
         try {
@@ -39,30 +40,29 @@ public class Journal_Service {
         }
     }
 
-    public ResponseEntity<List<Entity>> getuserJournals (String username) {
+    public ResponseEntity<List<Entity>> getuserJournals(String username) {
         Optional<User> exist = repoo.findByuserName(username);
         if (exist.isPresent()) {
             return ResponseEntity.ok(exist.get().getJournals());
         }
-     return ResponseEntity.notFound().build();
+        return ResponseEntity.notFound().build();
     }
 
-    public String getUserJournaldel(String username){
-        Optional<User> exist= repoo.findByuserName(username);
-     if (exist.isPresent()){
-         User user= exist.get();
-         String ss=username;
-         List<Entity> journl= user.getJournals();
-         journalrepo.deleteAll(journl);
-         repoo.delete(user);
-         return "deleted =" + ss;
-     }
-     return "notfound";
+    public String getUserJournaldel(String username) {
+        Optional<User> exist = repoo.findByuserName(username);
+        if (exist.isPresent()) {
+            User user = exist.get();
+            String ss = username;
+            List<Entity> journl = user.getJournals();
+            journalrepo.deleteAll(journl);
+            repoo.delete(user);
+            return "deleted =" + ss;
+        }
+        return "notfound";
     }
 
     public String deltealll() {
-        if(repoo.count()==0 && journalrepo.count()==0)
-        {
+        if (repoo.count() == 0 && journalrepo.count() == 0) {
             return "no element exist";
         }
         journalrepo.deleteAll();
@@ -70,12 +70,12 @@ public class Journal_Service {
         return "deleted all  element and journals";
     }
 
-    public String updateJournals(String username, ObjectId id, Entity journals){
-        Optional<User> exist= repoo.findByuserName(username);
-        if (exist.isPresent()){
-            User user= exist.get();
-            for(Entity search: user.getJournals()){
-                if(search.getId().equals(id)){
+    public String updateJournals(String username, ObjectId id, Entity journals) {
+        Optional<User> exist = repoo.findByuserName(username);
+        if (exist.isPresent()) {
+            User user = exist.get();
+            for (Entity search : user.getJournals()) {
+                if (search.getId().equals(id)) {
                     search.setTitle(journals.getTitle());
                     search.setContent(journals.getContent());
                     journalrepo.save(search);
@@ -86,23 +86,21 @@ public class Journal_Service {
         return " not found the user";
     }
 
-    public String deletebyId(ObjectId id, String username){
-        Boolean exist=false;
+    public String deletebyId(ObjectId id, String username) {
+        Boolean exist = false;
         try {
-         Optional<User> userExist = repoo.findByuserName(username);
-         User user= userExist.get();
-         exist = user.getJournals().removeIf(x->x.getId().equals(id));
-         if(exist){
-             journalrepo.deleteById(id);
-             repoo.save(user);
+            Optional<User> userExist = repoo.findByuserName(username);
+            User user = userExist.get();
+            exist = user.getJournals().removeIf(x -> x.getId().equals(id));
+            if (exist) {
+                journalrepo.deleteById(id);
+                repoo.save(user);
 
-         }
-
-
+            }
         } catch (Exception e) {
 
             throw new RuntimeException("An error occurred while deleting the entry.", e);
         }
-        return "deltede by id:" +exist;
+        return "deltede by id:" + exist;
     }
 }
